2aa58d300facaadccb237058c42b6352d467f8d3
Zhen Tang <zt438@nyu.edu>
2019-05-03 16:11:31 -0400

    added counter for number of sentences words appear in

--- Summary.java
+++ Summary.java
@@ -6,7 +6,14 @@ public class Summary {
 
     public static void main(String[] args) throws IOException{
     
-        String file = "doors.txt";
+        String file;
+        if (args.length == 0) {
+            file = "doors.txt";
+        }
+        else {
+            file = args[0];
+        }
+            
         BufferedReader reader = new BufferedReader(new FileReader(file));
         StringBuilder builder = new StringBuilder();
         String currentLine = reader.readLine();
@@ -34,7 +41,8 @@ public class Summary {
         int numSentences = sentences.size();
         
         Map<String, Integer> occurance = new HashMap<String, Integer>();
-
+        Map<String, Integer> sentenceCount = new HashMap<String, Integer>();
+        
         String[] words;
         for(int i = 0; i < sentences.size(); i++){
             int begin = sentences.get(i);
@@ -42,6 +50,9 @@ public class Summary {
                         ? str.length() : sentences.get(i + 1);
             String singleSentence = str.substring(begin, end);
             
+            // TODO: there's a better way to do this right?
+            ArrayList<String> currentSentence = new ArrayList<String>();
+                
             // remove punctuation and convert to lowercase and then split
             // on white spaces
             words = singleSentence.replaceAll("[^a-zA-Z ]", "")
@@ -53,6 +64,19 @@ public class Summary {
                 } else {
                     occurance.put(singleWord, 1);
                 }
+                
+                if (!currentSentence.contains(singleWord)) {
+                    currentSentence.add(singleWord);
+                }
+            }
+            
+            for (String word : currentSentence) {
+                if (sentenceCount.containsKey(word)) {
+                    int count = sentenceCount.get(word) + 1;
+                    sentenceCount.put(word, count);
+                } else {
+                    sentenceCount.put(word, 1);
+                }
             }
         }
         
@@ -63,10 +87,12 @@ public class Summary {
         list.sort(Map.Entry.comparingByValue());
 
         for(Map.Entry<String, Integer> entry : list){
-            System.out.println("key: " + entry.getKey() + "; value: " + entry.getValue());
+            System.out.printf("key: %s; occurences: %d; sentences: %d\n",
+                entry.getKey(), entry.getValue(),
+                sentenceCount.get(entry.getKey()));
         }
 
-        System.out.println(numSentences);
+        System.out.printf("Number of sentences: %d\n", numSentences);
     }
 
 }
